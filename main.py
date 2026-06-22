59a2ab4f798eb51218bb52e8db36d3263cbd8755
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 02:41:38 +0200

    Register multiple plugins in main.py

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+# أوامر البداية
 from plugins.start import register as start_register
 start_register(app)
 
+# الاشتراك الإجباري
 from plugins.force_sub import register as force_sub_register
 force_sub_register(app)
+
+# الإذاعات
+from plugins.broadcast import register as broadcast_register
+broadcast_register(app)
 
 print("HMD-ALSOURY-BEST Started")
 app.run()
